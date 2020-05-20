ac7979c0d2e7a7ae2486332fab89ac9699f5376a
YuqiCui <438049712@qq.com>
2020-05-20 15:24:22 +0800

    Update TSK_FS.py

--- TSK_FS.py
+++ TSK_FS.py
@@ -1,45 +1,43 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression, RidgeClassifier
-from fuzzy_cluster import ESSC
+from lib.fuzzy_cluster import ESSC
 from scipy.special import softmax
+import copy
 
-
-class TSKFuzzyClassifier:
-    def __init__(self, alpha=0, estimator='logistic', estimator_c=1., n_cluster=2, cluster_m=2, cluster_eta=0.1,
-                 cluster_gamma=0.1, max_iter=10, cluster_scale=1., order=1, min_cluster_sample=10, n_jobs=5, fl_threshold=0.2):
+class FuzzyBoostClassifier:
+    def __init__(self, estimator='logistic', estimator_c=1., n_cluster=2, cluster_m=2, cluster_eta=0.1,
+                 cluster_gamma=0.1, cluster_scale=1., order=1):
         """
 
-        :param n_cluster: number of clusters in each step
+        :param estimator: Classifier type, 'logistic' for LogisticRegression, 'ridge' for RidgeClassifier
+        :param estimator_c: L2 regularization for estimator, 'C' in LogisticRegression, 'alpha' in RidgeClassifier
+        :param n_cluster: number of clusters
+        :param cluster_m: fuzzy index $m$ for ESSC
+        :param cluster_eta: parameter $\eta$ for ESSC
+        :param cluster_gamma: parameter $\gamma$ for ESSC
+        :param cluster_scale: scale parameter for ESSC
+        :param order: 0 or 1, order of TSK
         """
-        self.alpha = alpha
         self.estimator = estimator
         self.estimator_c = estimator_c
         self.n_cluster = n_cluster
         self.cluster_m = cluster_m
         self.cluster_eta = cluster_eta
         self.cluster_gamma = cluster_gamma
-        self.max_iter = max_iter
         self.cluster_scale = cluster_scale
         self.order = order
-        self.min_cluster_sample = min_cluster_sample
-        self.n_jobs = n_jobs
-        self.fl_threshold = fl_threshold
+
 
     def get_params(self, deep=True):
         return {
-            'alpha': self.alpha,
             'estimator': self.estimator,
             'estimator_c': self.estimator_c,
             'n_cluster': self.n_cluster,
             'cluster_m': self.cluster_m,
             'cluster_eta': self.cluster_eta,
             'cluster_gamma': self.cluster_gamma,
-            'max_iter': self.max_iter,
             'cluster_scale': self.cluster_scale,
             'order': self.order,
-            'min_cluster_sample': self.min_cluster_sample,
-            'n_jobs': self.n_jobs,
-            'fl_threshold': self.fl_threshold
         }
 
     def set_params(self, **params):
@@ -80,31 +78,28 @@ class TSKFuzzyClassifier:
         else:
             return self.__logits_to_prob_multi_class__(out)
 
-    def __weighted_average_outs__(self, X, mem, experts):
-        assert mem.shape[1] == len(experts), "membership and experts length don't match"
-        if self.n_classes == 2:  # average outputs
-            outs = np.array([experts[i].decision_function(X) for i in range(len(experts))])  # [n_samples, n_clusters]
-            outs = np.average(outs, axis=0, weights=mem.T)
-            return self.__logits_to_prob_2_class__(outs)
-        else:
-            outs = np.array([experts[i].decision_function(X) for i in range(len(experts))])  # [n_samples, n_clusters]
-            print(outs.shape, mem.shape)
-            outs = self.__logits_to_prob_multi_class__(outs, axis=2)
-            outs = np.sum(outs * np.expand_dims(mem.T, axis=2), axis=0) / np.sum(mem, axis=1, keepdims=True)
-            return outs
-
     @staticmethod
     def __logits_to_prob_2_class__(logits):
+        """
+        Converting 2 class raw score to probability
+        :param logits:
+        :return:
+        """
         return 1/(1+np.exp(-logits))
 
     @staticmethod
     def __logits_to_prob_multi_class__(logits, axis=1):
+        """
+        Converting multi-class raw score to probability
+        :param logits:
+        :return:
+        """
         return softmax(logits, axis=axis)
 
     @staticmethod
     def __cluster__(data, n_cluster, eta, gamma, scale):
         """
-        Comute data centers and membership of each point by FCM, and compute the variance of each feature
+        Comute data centers and membership of each point by ESSC, and compute the variance of each feature
         :param data: n_Samples * n_Features
         :param n_cluster: number of center
         :return: centers: data center, delta: variance of each feature
@@ -121,7 +116,7 @@ class TSKFuzzyClassifier:
         :param data: n_Samples * n_Features
         :param centers: data center，n_Clusters * n_Features
         :param delta: variance of each feature， n_Clusters * n_Features
-        :return: data_fs data的firing strength, n_Samples * [n_Clusters * (n_Features+1)]
+        :return: firing strength
         """
         d = -(np.expand_dims(data, axis=2) - np.expand_dims(centers.T, axis=0))**2 / (2 * delta.T)
         d = np.exp(np.sum(d, axis=1))
@@ -130,6 +125,13 @@ class TSKFuzzyClassifier:
 
     @staticmethod
     def x2xp(X, mem, order=1):
+        """
+        Converting raw input feature X to TSK consequent input
+        :param X: raw input, [n_sample, n_features]
+        :param mem: firing level of each rule, [n_sample, n_clusters]
+        :param order:order of TSK, 0 or 1
+        :return:
+        """
         if order == 0:
             return mem
         else:
@@ -146,10 +148,9 @@ class TSKFuzzyClassifier:
 
         :param X:
         :param y:
-        :param mu_a: firing level
+        :param mu_a: firing level, [n_sample, n_clusters]
         :return:
         """
-        n_experts = mu_a.shape[1]
         if self.estimator == 'logistic':
             experts = LogisticRegression(C=self.estimator_c, max_iter=200)
         elif self.estimator == 'ridge':
